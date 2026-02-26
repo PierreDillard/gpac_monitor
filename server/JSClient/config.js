89620ec1778260060245b18142a84fafcebc3c63
PierreDillard <7gaspard77@gmail.com>
2026-02-26 15:34:59 +0100

    test ga update

--- server/JSClient/config.js
+++ server/JSClient/config.js
@@ -26,17 +26,6 @@ const UPDATE_INTERVALS = {
     CPU_STATS: 500,
 };
 
-// Log retention strategy: preserve errors (100%), prioritize warnings (80%), sample info/debug (20%/5%)
-const LOG_RETENTION = {
-    maxHistorySize: 500,
-    maxHistorySizeVerbose: 2000,
-    keepRatio: {
-        error: 1.0,
-        warning: 0.8,
-        info: 0.2,
-        debug: 0.05
-    }
-};
 
 export {
     DEFAULT_FILTER_FIELDS,
